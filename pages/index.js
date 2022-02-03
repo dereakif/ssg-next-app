a3aaa2bea2d8426246df4415363992ce3b5d5791
Akif <dereakif@gmail.com>
2022-02-04 01:12:31 +0300

    implement: useQuery with search api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import axios from "axios";
 import Head from "next/head";
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import { Card, Col, Container, FormControl, Row } from "react-bootstrap";
+import { useQuery } from "react-query";
 import styles from "../styles/Home.module.css";
 
+const getPokemon = async (filter) => {
+  const pokemonName = filter.queryKey[1];
+  const { data } = await axios.get(`/api/search?q=${pokemonName}`);
+  return data.map((pokemon) => ({
+    ...pokemon,
+    image: `/pokemon/${pokemon.name.english
+      .toLowerCase()
+      .replace(" ", "-")}.jpg`,
+  }));
+};
 export default function Home() {
+  const [query, setQuery] = useState("");
+  const { data } = useQuery(["q", query], getPokemon);
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +26,31 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container>
-        <h1>Hello</h1>
+        <FormControl
+          placeholder="search"
+          aria-label="Search"
+          value={query}
+          onChange={(evt) => setQuery(evt.target.value)}
+        />
+        {data && (
+          <Row>
+            {data.map(({ id, name, type, image }) => (
+              <Col xs={4} key={id} style={{ padding: 5 }}>
+                <Card>
+                  <Card.Img
+                    variant="top"
+                    style={{ maxHeight: 300 }}
+                    src={image}
+                  />
+                  <Card.Body>
+                    <Card.Title>{name.english}</Card.Title>
+                    <Card.Subtitle>{type.join(", ")}</Card.Subtitle>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        )}
       </Container>
     </div>
   );
