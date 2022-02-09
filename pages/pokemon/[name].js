566611f03c41d269614201e0fa3dd6830d17ebaa
Akif Dere <dereakif@gmailcom>
2022-02-09 19:44:12 +0300

    render: pokemon data and image

--- pages/pokemon/[name].js
+++ pages/pokemon/[name].js
@@ -1,5 +1,7 @@
 import axios from "axios";
+import Head from "next/head";
 import { useRouter } from "next/router";
+import { Col, Container, Row } from "react-bootstrap";
 import { useQuery } from "react-query";
 
 const getPokemon = async (filter) => {
@@ -11,5 +13,36 @@ const getPokemon = async (filter) => {
 export default () => {
   const router = useRouter();
   const { data } = useQuery(["name", router.query.name], getPokemon);
-  return <div>{data && <div>{data.id}</div>}</div>;
+  return (
+    <div>
+      <Head>
+        <title>{data && data.name.english}</title>
+      </Head>
+      <Container>
+        {data && (
+          <>
+            <h1>{data.name.english}</h1>
+            <Row>
+              <Col xs={4}>
+                <img
+                  src={`/pokemon/${data.name.english
+                    .toLowerCase()
+                    .replace(" ", "-")}.jpg`}
+                  style={{ width: "100%" }}
+                />
+              </Col>
+              <Col xs={8}>
+                {Object.entries(data.base).map(([key, value]) => (
+                  <Row key={key}>
+                    <Col xs={2}>{key}</Col>
+                    <Col xs={10}>{value}</Col>
+                  </Row>
+                ))}
+              </Col>
+            </Row>
+          </>
+        )}
+      </Container>
+    </div>
+  );
 };
